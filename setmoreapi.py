1913dc99ae67249b02d6c51aa20a3b0e9311b69a
Dylan Funk <dwf33@case.edu>
2020-08-14 12:49:40 -0400

    Appointment Parser Complete

--- setmoreapi.py
+++ setmoreapi.py
@@ -24,5 +24,33 @@ def appointmentGetter():
         headers = {
             "Authorization": "Bearer "+ tokenGetter(),
             "Content-Type" : "application/json"})
-    return appointments.json()["data"]["appointments"]
-print(appointmentGetter())
+    try:
+        results = appointments.json()["data"]["appointments"]
+    except:
+        print("ERROR")
+        results = "Error"
+    return results
+
+def appointmentParser(appointments):
+    dailySchedule = []
+    for aptInfo in appointments: #dictionary comprehension seemed too complicated with nested dictionaries
+        appointment = {
+            'date': aptInfo["start_time"][0:10], 
+            'time': aptInfo["start_time"][11:],
+            'duration': aptInfo["duration"], 
+            'first': aptInfo["customer"]["first_name"],
+            'last': aptInfo["customer"]["last_name"],
+            'email': aptInfo["customer"]["email_id"],
+            'phone': aptInfo["customer"]["cell_phone"],
+            'address': aptInfo["customer"]["address"],
+            'city': aptInfo["customer"]["city"],
+            'state': aptInfo["customer"]["state"],
+            'zip': aptInfo["customer"]["postal_code"],
+            'dob': aptInfo["customer"]["additional_fields"]["Date of Birth"],
+            'gender': aptInfo["customer"]["additional_fields"]["Gender"],
+            'label': aptInfo["label"]}
+        dailySchedule.append(appointment)
+    return dailySchedule
+
+print([list(apt.values()) for apt in appointmentParser(appointmentGetter())])
+        
