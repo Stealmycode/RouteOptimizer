7ff33082bf4337b6e7d2ed4d7f6e7deea5a96d15
Dylan Funk <dwf33@case.edu>
2020-08-14 13:42:00 -0400

    Google Sheets API requests.post() draft

--- setmoreapi.py
+++ setmoreapi.py
@@ -1,6 +1,6 @@
 import datetime
 import requests
-from config import setmoreRefresh
+from config import setmoreRefresh, homeSheetID
 
 def datestring(date):
     x = str(date).split("-")
@@ -12,14 +12,12 @@ def tokenGetter():
     accessToken = accessResponse.json()["data"]["token"]["access_token"]
     return accessToken
 
-def appointmentGetter():
-    today = datetime.date.today()
+def appointmentGetter(today):
     tomorrow = today + datetime.timedelta(days = 1)
     appointments = requests.get(
-        "https://developer.setmore.com/api/v1/bookingapi/appointments?startDate=" 
-        + datestring(today) 
-        + "&endDate=" 
-        + datestring(tomorrow) 
+        "https://developer.setmore.com/api/v1/bookingapi/appointments?"
+        + " startDate=" + datestring(today) 
+        + "&endDate=" + datestring(tomorrow) 
         + "&customerDetails=true",
         headers = {
             "Authorization": "Bearer "+ tokenGetter(),
@@ -52,5 +50,21 @@ def appointmentParser(appointments):
         dailySchedule.append(appointment)
     return dailySchedule
 
-print([list(apt.values()) for apt in appointmentParser(appointmentGetter())])
-        
+def sheetMaker(today):
+    values = [list(apt.values()) for apt in appointmentParser(appointmentGetter(today))]
+    requests.put(
+        "https://sheets.googleapis.com/v4/spreadsheets/"\
+        + homeSheetID + "/values/A2%3AN7?"\
+        "responseDateTimeRenderOption=FORMATTED_STRING"\
+        "&responseValueRenderOption=UNFORMATTED_VALUE"\
+        "&valueInputOption=RAW"\
+        + "&key=" + [YOUR_API_KEY], #we need OAuth 2.0 access tokens and a refresh token since we're access private data
+        headers = {
+            'Authorization' : 'Bearer [YOUR_ACCESS_TOKEN]',
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'},
+        data = values)
+
+today = datetime.date.today()
+print(today)
+#sheetMaker(today)
